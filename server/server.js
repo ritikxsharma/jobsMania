0cab109b7a7ce0e153b2e01e1e35fafa7ae8bf0e
Ritik Sharma <sharmaritik2002@gmail.com>
2025-02-16 23:40:40 +0530

    fixed cors error

--- server/server.js
+++ server/server.js
@@ -22,8 +22,10 @@ connectDB()
     app.use(cookieParser());
     app.use(
       cors({
-        origin: "http://localhost:5173",
+        origin: "*",
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"]
       })
     );
 
